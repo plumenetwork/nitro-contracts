c4521b4cc14c15144944b69c543b184b99cd64e0
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-01-26 19:54:54 -0500

    add ArbHi precompile at 0x11a

---
+++ src/precompiles/ArbHi.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.21 <0.9.0;
+
+/**
+ * @title Say hi.
+ * @notice just for test
+ * This custom contract will set on 0x000000000000000000000000000000000000011a since we set it in precompile.go.
+ */
+interface ArbHi {
+    function sayHi() external view returns(string memory);
+}
