5b93471b793aee5af259695e2b34d617fa5041a8
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-01-27 21:01:46 -0500

    add Hi event to ArbHi precompile

--- src/precompiles/ArbHi.sol
+++ src/precompiles/ArbHi.sol
@@ -6,5 +6,7 @@ pragma solidity >=0.4.21 <0.9.0;
  * This custom contract will set on 0x000000000000000000000000000000000000011a since we set it in precompile.go.
  */
 interface ArbHi {
-    function sayHi() external view returns(string memory);
+    function sayHi() external returns(string memory);
+
+    event Hi(address indexed caller);
 }
